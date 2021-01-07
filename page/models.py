948ced1e9c8d6305fe33749737d9431e0a548764
juungbae <juungbae@gmail.com>
2021-01-07 20:38:23 +0900

    docs: TODO 주석 추가 / PEP8에 맞게 코드 수정

--- page/models.py
+++ page/models.py
@@ -3,11 +3,13 @@ from django.utils import timezone
 from django.conf import settings
 
 # Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=50, default='')
     update_date = models.DateTimeField(default=timezone.now)
     text = models.TextField()
-    image = models.ImageField(upload_to = 'images/', blank = True, null = True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
--- page/views.py
+++ page/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 
 # Create your views here.
+# TODO: 각각의 view가 어떤 걸 의미하는지 Docstring으로 적어주세요.
+
+
 def home(request):
     posts = Post.objects.all()
-    return render(request, 'home.html', {'posts':posts})
+    return render(request, 'home.html', {'posts': posts})
+
 
 def detail(request, post_id):
-    post = get_object_or_404(Post, pk = post_id)
-    return render(request, 'detail.html', {'post':post})
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'detail.html', {'post': post})
+
 
 def new(request):
     return render(request, 'new.html')
+
 
 def create(request):
     if request.method == 'POST':
@@ -24,9 +30,10 @@ def create(request):
         post.save()
 
         return redirect('detail', post.id)
-    
+
+
 def update(request, post_id):
-    post = get_object_or_404(Post, pk = post_id)
+    post = get_object_or_404(Post, pk=post_id)
 
     if request.method == 'POST':
         if 'image' in request.FILES:
@@ -37,9 +44,10 @@ def update(request, post_id):
         post.save()
         return redirect('detail', post.id)
     else:
-        return render(request, 'update.html', {'post':post})
+        return render(request, 'update.html', {'post': post})
+
 
 def delete(request, post_id):
-    post = get_object_or_404(Post, pk = post_id)
+    post = get_object_or_404(Post, pk=post_id)
     post.delete()
     return redirect('home')
